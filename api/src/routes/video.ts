832aaec717f0668b8f1ca80d15d3f120f76c9595
Pyrex <retojonatan@gmail.com>
2021-07-02 21:15:55 -0300

    added db schema

---
+++ api/src/routes/video.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    url: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default model("video", videoSchema);
