0bd05c1aa1620d3279b52feb0b4f77126043a969
Pyrex <retojonatan@gmail.com>
2021-07-03 17:29:05 -0300

    changed port for the api config

--- api/src/config.ts
+++ api/src/config.ts
@@ -6,5 +6,5 @@ export default {
   MONGO_USER: process.env.MONGO_USER || "user",
   MONGO_PASSWORD: process.env.MONGO_PASSWORD || "password",
   MONGO_HOST: process.env.MONGO_HOST || "localhost",
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 4000,
 };
