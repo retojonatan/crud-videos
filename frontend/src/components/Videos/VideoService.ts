1f35d9ef301de329c72fb9ca36b55a2e9e3a1126
Pyrex <retojonatan@gmail.com>
2021-07-03 17:36:55 -0300

    added video service for axios requests

--- frontend/src/components/Videos/VideoService.ts
+++ frontend/src/components/Videos/VideoService.ts
@@ -6,6 +6,7 @@ const API = "http://localhost:4000";
 export const getVideos = async () => {
   return await axios.get(`${API}/videos`);
 };
+
 export const createVideo = async (video: Video) => {
   return await axios.post(`${API}/videos`, video);
 };
