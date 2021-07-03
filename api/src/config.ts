9bd7ea99312bcb8063a7999f2d3965314cef179f
Pyrex <retojonatan@gmail.com>
2021-07-02 21:14:08 -0300

    added dot env on config module

---
+++ api/src/config.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  MONGO_DB: process.env.MONGO_DB || "dbVideos",
+  MONGO_USER: process.env.MONGO_USER || "user",
+  MONGO_PASSWORD: process.env.MONGO_PASSWORD || "password",
+  MONGO_HOST: process.env.MONGO_HOST || "localhost",
+  PORT: process.env.PORT || 3000,
+};
