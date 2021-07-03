5ad5e2537e86071e5d503e8115b6c211df474765
Pyrex <retojonatan@gmail.com>
2021-07-03 17:30:54 -0300

    created video item

---
+++ frontend/src/components/Videos/VideoItem.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Video from "./Video";
+
+interface Props {
+  video: Video;
+}
+
+const VideoItem = ({ video }: Props) => {
+  return (
+    <div>
+      <h1>{video.title}</h1>
+      <p>{video.description}</p>
+    </div>
+  );
+};
+
+export default VideoItem;
