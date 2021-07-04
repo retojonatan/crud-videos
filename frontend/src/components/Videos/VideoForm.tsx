7b92585aa1fd3eb631c4685e18b04338b8bb69d8
Pyrex <retojonatan@gmail.com>
2021-07-04 20:10:57 -0300

    added videoForm to handle create and update videos

--- frontend/src/components/Videos/VideoForm.tsx
+++ frontend/src/components/Videos/VideoForm.tsx
@@ -1,8 +1,14 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
-import { useHistory } from "react-router-dom";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import Video from "./Video";
 import * as videoService from "./VideoService";
+
+type InputChange = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
+
+interface Params {
+  id: string;
+}
 
 const VideoForm = () => {
   const initialState = {
@@ -12,10 +18,14 @@ const VideoForm = () => {
   };
 
   const [video, setVideo] = useState<Video>(initialState);
-
-  type InputChange = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
-
   const history = useHistory();
+  const params = useParams<Params>();
+
+  const getVideo = async (id: string) => {
+    const res = await videoService.getVideo(id);
+    const { title, description, url } = res.data;
+    setVideo({ title, description, url });
+  };
 
   const handleInputChange = (e: InputChange) => {
     setVideo({ ...video, [e.target.name]: e.target.value });
@@ -23,19 +33,30 @@ const VideoForm = () => {
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    await videoService.createVideo(video);
-    toast.success("New video added");
+    if (!params.id) {
+      await videoService.createVideo(video);
+      toast.success("New video added");
+    } else {
+      await videoService.updateVideo(params.id, video);
+      toast.success("Video updated");
+    }
+    // setVideo(initialState);
     history.push("/");
   };
 
+  useEffect(() => {
+    if (params.id) getVideo(params.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="row">
-      <div className="col-md-4 offset-md-4">
+      <div className="col-md-6 offset-md-3">
         <div className="card">
           <div className="card-body">
             <h3>New Video</h3>
             <form onSubmit={handleSubmit}>
-              <div className="form-group">
+              <div className="form-group my-2">
                 <input
                   className="form-control"
                   type="text"
@@ -46,7 +67,7 @@ const VideoForm = () => {
                   autoFocus
                 />
               </div>
-              <div className="form-group">
+              <div className="form-group my-2">
                 <input
                   className="form-control"
                   type="text"
@@ -56,7 +77,7 @@ const VideoForm = () => {
                   value={video.url}
                 />
               </div>
-              <div className="form-group">
+              <div className="form-group my-2">
                 <textarea
                   className="form-control"
                   rows={3}
@@ -66,7 +87,11 @@ const VideoForm = () => {
                   value={video.description}
                 />
               </div>
-              <button className="btn btn-primary">Create Video</button>
+              {params.id ? (
+                <button className="btn btn-info">Update Video</button>
+              ) : (
+                <button className="btn btn-primary">Create Video</button>
+              )}
             </form>
           </div>
         </div>
